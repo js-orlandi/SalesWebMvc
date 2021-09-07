bbe0d4d130006cf4ac387e95d8d752f0425cfebf
Juliano S Orlandi <js.orlandi.dev@gmail.com>
2021-09-07 20:38:46 -0300

    First controler and Razor pages tests

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
+using System; 
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
